c2ac75307a32294b179f561a0c4269660bef7818
Maksim <maksim.parkhomenko@icloud.com>
2019-12-27 13:23:41 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,39 @@
-var gulp = require('gulp'),
+let {task, dest, src, watch, series} = require('gulp'),
     miniCSS = require('gulp-minify-css'),
     renameFile = require('gulp-rename'),
     autoprefixer = require('gulp-autoprefixer'),
-    jsmin = require('gulp-jsmin');
+    jsmin = require('gulp-jsmin'),
+    cleanCSS = require('gulp-clean-css');
 
-gulp.task('css', function() {
-    return gulp.src('css/style.css')
-        .pipe(miniCSS('style.css'))
-        .pipe(renameFile('style.min.css'))
+task('css-main', () => {
+    return src('css/main.css')
+        .pipe(miniCSS('main.css'))
+        .pipe(cleanCSS('main.css'))
+        .pipe(renameFile('main.min.css'))
         .pipe(autoprefixer('last 15 versions'))
-        .pipe(gulp.dest('css/'))
+        .pipe(dest('css/'))
 });
 
-gulp.task('javascript', function () {
-    return gulp.src('js/script.js')
-        .pipe(jsmin('script.js'))
-        .pipe(renameFile('script.min.js'))
-        .pipe(gulp.dest('js/'))
+task('css-mobile', () => {
+    return src('css/main-mobile.css')
+        .pipe(miniCSS('main-mobile.css'))
+        .pipe(cleanCSS('main-mobile.css'))
+        .pipe(renameFile('main-mobile.min.css'))
+        .pipe(autoprefixer('last 15 versions'))
+        .pipe(dest('css/'))
 });
 
-gulp.task('watch', function () {
-    gulp.watch('css/style.css', ['css'])
-    gulp.watch('js/script.js', ['javascript'])
+task('javascript', () => {
+    return src('js/functions.js')
+        .pipe(jsmin('functions.js'))
+        .pipe(renameFile('functions.min.js'))
+        .pipe(dest('js/'))
 });
+
+task('watch', () => {
+    watch('css/main.css', series('css-main'))
+    watch('js/functions.js', series('javascript'))
+    watch('css/main-mobile.css', series('css-mobile'))
+});
+
+exports.default = series('css-main', 'javascript', 'css-mobile');
